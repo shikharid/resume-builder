a0b2fe5b26353a6c9a5b27cdaf6216b0a6197be8
shikharid <shikharid@gmail.com>
2015-08-20 07:30:31 +0530

    Delete edit.php

--- edit.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$value = $_POST["content"];
-	$name = $_POST["name"];
-	$name = "edit/".substr($name,strrpos($name,'/') + 1);
-	file_put_contents($name, $value);
-	header( "url=edit/test.html".$name);
-	echo $name;
-?>
